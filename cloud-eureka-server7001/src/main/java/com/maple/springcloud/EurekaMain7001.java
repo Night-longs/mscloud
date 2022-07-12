b3146a7ca849bf6208300174eacc31518b11e0f8
Night-longs <243739213@qq.com>
2022-07-12 20:00:30 +0800

    [cloud-003] Eureka单机版搭建

--- cloud-eureka-server7001/src/main/java/com/maple/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/maple/springcloud/EurekaMain7001.java
@@ -2,18 +2,17 @@ package com.maple.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Package_Name: com.maple.springcloud
  * @Auth: Maple
- * @Date: 2022/7/12 9:40
+ * @Date: 2022/7/12 18:59
  **/
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
-
 }
--- cloud-provider-payment8001/src/main/java/com/maple/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/maple/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.maple.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Package_Name: com.maple.springcloud
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2022/7/11 14:18
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
