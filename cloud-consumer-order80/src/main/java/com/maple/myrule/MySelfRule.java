0e4936ae85e87471bf45a7ddad6d4f45deb237cd
Night-longs <243739213@qq.com>
2022-07-22 14:35:18 +0800

    [cloud-011] 1.Ribbon是负载均很加RestTemplate调用             2.更改Ribbon的负载均衡算法，默认为轮询，修改为随机

---
+++ cloud-consumer-order80/src/main/java/com/maple/myrule/MySelfRule.java
@@ -0,0 +1,19 @@
+package com.maple.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Package_Name: com.maple.myrule
+ * @Auth: Maple
+ * @Date: 2022/7/22 14:22
+ **/
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule getRule() {
+        return new RandomRule();
+    }
+}
